a513b1211d40523d33ed91bdd2b7c8482b04be48
José Pino <fraphcore@gmail.com>
2014-01-31 17:56:06 -0500

    toptext.js
    toptext.js

---
+++ toptext.js
@@ -0,0 +1,13 @@
+/*
+* toptext.js
+* Upload a range higher and lower one smaller range
+* Sube un rango mayor  y baja un rango menor
+* by @mrjopino
+* for preytext
+*/
+function Toptext(p, t) {
+  return (parseInt(- $(p).find("#vistas").text(), 10) - parseInt(- $(t).find("#vistas").text(), 10));
+} 
+$("#top").click(function() {
+$("section").sort(Toptext).appendTo("main");    
+});
